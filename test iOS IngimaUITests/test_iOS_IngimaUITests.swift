21e52ebb817b0875e315720b25d919f4fc8fe05e
Loïc Sillere <loic.sillere@gmail.com>
2017-03-15 19:58:25 +0100

    Initial Commit

---
+++ test iOS IngimaUITests/test_iOS_IngimaUITests.swift
@@ -0,0 +1,36 @@
+//
+//  test_iOS_IngimaUITests.swift
+//  test iOS IngimaUITests
+//
+//  Created by Loic Sillere on 15/03/2017.
+//  Copyright © 2017 Loic Sillere. All rights reserved.
+//
+
+import XCTest
+
+class test_iOS_IngimaUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
